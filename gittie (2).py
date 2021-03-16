b08d13e53464c77f7dcb5086d26d0f3664b8c993
Roman Pindela <roman.pindela@gmail.com>
2021-03-16 09:05:54 +0100

    Init repo.

---
+++ gittie (2).py
@@ -0,0 +1,22 @@
+def introduce():
+    print("Hello, I'm Gittie !")
+    print("Hello I'm Lucas")
+    print("Hey, This's Roman's version of code line 1")
+    print("Roman's part 2")
+    print("Roman: Here we go again :)")
+
+
+def joke():
+    print("It's a joke")
+
+
+def add(a, b):
+    return a + b
+
+
+def shout(message):
+    print(message)
+    return introduce()
+
+
+shout('Hello, Hey')
